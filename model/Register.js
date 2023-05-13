47ed90a14a6b5763ea8399b69329a35c5f98f817
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2023-05-13 17:53:54 +0530

    Update Register.js

--- model/Register.js
+++ model/Register.js
@@ -13,6 +13,10 @@ const schemaModel= new mongoose.Schema({
     email:{
         type:String,
         unique:true
+    },
+    role:{
+        type: String,
+        enum:["Admin" ,"SuperAdmin" , "User"]
     }
 
 },{timestamps:true})
